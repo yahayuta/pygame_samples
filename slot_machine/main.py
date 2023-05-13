ce056569cd39cfab2d42752c26c9ba82e54746fa
やはゆた <13937582+yahayuta@users.noreply.github.com>
2023-05-13 16:03:00 +0900

    basic score system built

--- slot_machine/main.py
+++ slot_machine/main.py
@@ -4,6 +4,12 @@ import random
 # Initialize Pygame
 pygame.init()
 
+# Initialize Pygame mixer
+pygame.mixer.init()
+
+# Load sound file
+start_sound = pygame.mixer.Sound('sound_files/start.mp3')
+win_sound = pygame.mixer.Sound('sound_files/win.wav')
 
 # Load background images
 background = pygame.image.load("image_files/background.jpg")
@@ -16,15 +22,27 @@ pygame.display.set_caption("Slot Machine")
 
 # Load images
 reel_images = [
-    pygame.image.load("image_files/bell.jpg"),
+    pygame.image.load("image_files/seven.jpg"),
     pygame.image.load("image_files/cherry.jpg"),
     pygame.image.load("image_files/plum.jpg"),
-    pygame.image.load("image_files/seven.jpg"),
     pygame.image.load("image_files/wm.jpg"),
+    pygame.image.load("image_files/bell.jpg"),
 ]
 spin_button = pygame.image.load("image_files/spin.png")
 spin_button_rect = spin_button.get_rect()
 spin_button_rect.center = (WIDTH // 2, HEIGHT - 20)
+
+combinations = [
+    (0, 0, 0, 100),  # Three seven
+    (1, 1, 1, 50),  # Three cherries
+    (2, 2, 2, 25),  # Three wm
+    (3, 3, 3, 10),  # Three plums
+    (4, 4, 4, 5),  # Three bells
+    # (0, 0, -1, 2),  # Two sevens and anything
+    # (0, -1, -1, 1),  # One seven and anything
+    # (1, 1, -1, 1),  # Two cherries and anything
+    # (1, -1, -1, 0.5),  # One cherrie and anything
+]
 
 # Define constants
 REEL_WIDTH = reel_images[0].get_width()
@@ -50,6 +68,7 @@ class Reel:
         self.spin_duration = random.randint(20, 40)
         self.spin_time = 0
         self.stopped = False
+        start_sound.play()
     
     def update(self):
         if not self.stopped:
@@ -80,6 +99,10 @@ BLACK = (0, 0, 0)
 
 # Define the game loop
 def game_loop():
+    all_stop = True
+    total = 0
+    message = ''
+    win = False
     running = True
     while running:
         # Handle events
@@ -87,9 +110,11 @@ def game_loop():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if spin_button_rect.collidepoint(pygame.mouse.get_pos()):
+                if all_stop and spin_button_rect.collidepoint(pygame.mouse.get_pos()):
                     for reel in reels:
                         reel.start_spin()
+                    all_stop = False
+                    win = False
         
         # Update the game state
         for reel in reels:
@@ -102,9 +127,19 @@ def game_loop():
         screen.blit(spin_button, spin_button_rect)
 
         # Check for a win
-        if reels[0].stopped == True and reels[1].stopped == True and reels[2].stopped == True and reels[0].current_index == reels[1].current_index == reels[2].current_index:
-            message = font.render("You Win!", True, BLACK)
-            screen.blit(message, (WIDTH / 4, 0))
+        if all_stop == False and reels[0].stopped == True and reels[1].stopped == True and reels[2].stopped == True:
+            start_sound.stop()
+            all_stop = True
+            for combination in combinations:
+                if combination[0] == reels[0].current_index and combination[1] == reels[1].current_index and combination[2] == reels[2].current_index:
+                    total += combination[3]
+                    win_sound.play()
+                    win = True
+                    str_message = f"Win! {str(combination[3])}, total:{str(total)}"
+                    message = font.render(str_message, True, BLACK)
+
+        if all_stop and win:
+            screen.blit(message, (0, 0))
 
         pygame.display.flip()
 
